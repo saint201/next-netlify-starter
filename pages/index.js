37ca7816106c73477cdcf58a2064c2542e80cf2e
saint201 <89702989+saint201@users.noreply.github.com>
2021-12-17 18:13:04 +0300

    index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,57 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+   <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+<title>death eye</title>
+</head>
+<body>
+<table border="1" align="center" rules="rows" style="width:60%;">
+<!--Создаём строку-->
+<tbody><tr>
+<!--Создаём ячейку строки-->
+<td>
+<!--ШАПКА САЙТА-->
 
-      <Footer />
-    </div>
+<!--В ячейке строки создаём ещё одну таблицу для шапки сайта.
+Оформление:
+border="1" - двойная рамка толщиной в 1px
+background="images/168.png" - картинка в шапке сайта, если требуется.
+Адрес картинки вы должны вставить свой.
+bgcolor="#7FFFD4" - фоновый цвет в шапке, если нет картинки.
+cellpadding="10" - отступ содержимого от рамки не менее 10px.
+style="width:100%; border-radius:5px;" - добавляем "резиновость"
+и закругляем уголки рамки-->
+<table border="1" background="file:///C:/Users/saint/Desktop/images/168.png" bgcolor="#ff2400" cellpadding="10" style="width:100%; border-radius:5px;">
+<!--Создаём строку таблицы-->
+<tbody><tr>
+<!--Создаём столбец таблицы-->
+<th>
+<!--Содержание ячейки столбца-->
+<h1>DEATH EYE</h1>
+<h3 id="njrgddn234refg3orgnvng">#SFSKFk224kv04k4fudmr5j9jrjsd8hhjHU9HW9yw3fc14g5456w</h3>
+<!--Закрываем таблицу-->
+</th>
+</tr>
+</tbody></table>
+
+<!--ОСНОВНОЙ КОНТЕНТ-->
+
+<!--В этой же ячейке контейнера создаём ещё одну таблицу
+для основного контента.
+Оформление как и в предыдущей таблице-->
+
+
+
+<!--ПОДВАЛ-->
+
+<!--Создаём таблицу подвала-->
+
+<!--Закрываем таблицу контейнера-->
+</td>
+</tr>
+</tbody></table>
+
+</body>
   )
 }
